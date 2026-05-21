4adb1f58fb800c4ff76d18341058f4e9a17f864b
Valerii Navalnyi <valeranavalni@gmail.com>
2026-05-21 13:37:24 +0100

    Q1(g) Extend DBContext. Implement proper .gitignore

---
+++ OODExam_ValeriiNavalnyi/ClubData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODExam_ValeriiNavalnyi
+{
+    // for DB context to be extended
+    // Nu Get entity framework installed
+    public class ClubData : DbContext
+    {
+        // overload constructor
+        public ClubData() : base("OODExam_ValeriiNavalnyi") { } // the name of DB
+
+        // Creates Members table in DB
+        public DbSet<Member> Members { get; set; }
+
+        // Creates TrainingSessions table in DB
+        public DbSet<TrainingSession> TrainingSessions { get; set; }
+    }
+
+}
