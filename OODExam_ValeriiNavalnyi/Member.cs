781b26726b838234ae6c8e46497bcbc3784f9439
Valerii Navalnyi <valeranavalni@gmail.com>
2026-05-21 13:28:49 +0100

    Q1(c) and (d) done. Add classes

---
+++ OODExam_ValeriiNavalnyi/Member.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODExam_ValeriiNavalnyi
+{
+    public class Member
+    {
+
+        public int MemberId { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string ContactNumber { get; set; }
+        public string MembershipType { get; set; }
+
+
+    }
+}
---
+++ OODExam_ValeriiNavalnyi/TrainingSession.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODExam_ValeriiNavalnyi
+{
+    public class TrainingSession
+    {
+
+        public int SessionId { get; set; }
+        public DateTime SessionDate { get; set; }
+        public string SessionType { get; set; }
+        public int DurationMinutes { get; set; }
+        public string CoachNotes { get; set; }
+    }
+}
